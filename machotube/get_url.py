6151e0d5cffc2c3646e6bcc44bb851de3499d1fb
kqmvmh <kqmvmh8453@gmail.com>
2020-02-18 21:40:01 +0800

    update

--- machotube/get_url.py
+++ machotube/get_url.py
@@ -1,11 +1,43 @@
 # -*- coding: UTF-8 -*-
-#import pymongo
+import pymongo
 import requests
-#import lxml
+import lxml
 
 import common.setting as dl_setting
 import untils.setting as untils_setting
 
 machourl = dl_setting.macho_url
+mongo_ip = untils_setting.mongo
 print(machourl)
-# def get_ids(url, searchword, page):
+
+def get_ids(searchword, page):
+    if searchword == None:
+        url = machourl + '/newest?page=%s' % (str(page))
+    else:
+        url = machourl + '/search/%s/newest?page=%s' % (str(searchword), str(page))
+    res1 = requests.request("GET",url)
+    selector = etree.HTML(res1.text)
+    vlist = selector.xpath('//div[@class="b-thumb-list js-gallery-list"]/div[@class="b-thumb-list__wrap"]/div[@class="b-thumb-item"]')
+    #downedlist= ['4294530','4294530','4294530','1288419','5950251','2843962','6360080','1556151','7779463']
+    i=0
+    for div_i in vlist:
+        pid = div_i.xpath('./div/a/@data-position')
+        dataid = div_i.xpath('./div/a/@data-galleryid')
+        datathumbid = div_i.xpath('./div/a/@data-thumb-id')
+        filename = div_i.xpath('./div/a/@title')[0]
+        nexturl = "https://www.machotube.tv" + div_i.xpath('./div/a/@href')[0]
+        print(nexturl)
+        res_i = requests.request("GET",nexturl)
+        selector_i = etree.HTML(res_i.text)
+        url_i = selector_i.xpath('.//source/@src')[0]
+        print(url_i)
+        print("%s open down" %filename)
+        v_info = {
+            "filename": filename,
+            "v_url": nexturl,
+            "pid": str(pid),
+            "data_id": str(dataid),
+            "datathumbid": str(datathumbid),
+            "dl_url": url_i,
+            "dl_res": "0"
+        }
